80180064e5a26f87c3e40ddd6c2c8d3ffea3322d
Simon Gosse <simongosse.etude@gmail.com>
2025-01-13 18:44:05 +0100

    Updated Portfolio Title

--- islands/AppBar.tsx
+++ islands/AppBar.tsx
@@ -25,13 +25,13 @@ export default function AppBar() {
   }, []);
 
   return (
-    <nav class="flex justify-between items-center p-2">
+    <nav class="flex justify-between items-center p-2 mt-12">
       <a href="/" class="text-xl font-bold flex items-center">
         <Icon
           width="none"
           height="none"
         />
-        Portfolio
+        Simon Gosse Portfolio
       </a>
       <ul class="flex gap-2">
         <li>
